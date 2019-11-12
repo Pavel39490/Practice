ce7cf8aeba6717631a5389ecdd603b28243e064a
Pavel39490 <Pavel.Ahmed63@qmail.com>
2019-11-12 15:13:18 -0500

    I canged code in Cat

--- encapsulation/Cat.java
+++ encapsulation/Cat.java
@@ -7,6 +7,14 @@ public class Cat extends Animal
         Cat obj = new Cat ();
         System.out.println("Hey this is Pavelllll");
         obj.sounds();
+        //I'll add this Code now 
+        int a = 10;
+        int b = 20;
+        int add = a+b;
+
+        System.out.println(add);
+
+
     }
 
 }
